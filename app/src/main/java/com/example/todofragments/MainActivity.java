bc48cca395c6459c16e8e0a5cfd05fbee9ce520a
“Nathan” <nathw014@gmail.com>
2018-11-15 12:45:23 +0000

    Version 4.0 - Fragment Manager
    Added the fragment manager to the main activity, the todo list now functions as intended for the exercise.
    This intention is to simply get a fragment working, the checkbox is functional and the user can type in a text box too.

--- app/src/main/java/com/example/todofragments/MainActivity.java
+++ app/src/main/java/com/example/todofragments/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.todofragments;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Above was the initial code, now below is the code code added by the lab
+        // Starting of by creating a FragmentManager to manager our todo fragment
+        // that we have created
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        // If fragment is null... This is to do with saved instance state?
+        // So the bundle contains the id?
+        if (fragment == null){
+            TodoFragment todoFragment = new TodoFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, todoFragment)
+                    .commit();
+        }
     }
 }
